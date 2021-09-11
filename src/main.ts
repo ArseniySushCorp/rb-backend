2a89fcb5e67c15c915d00c5e8f6a8ec8c6bc5efc
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2021-09-11 14:51:43 +0300

    add module alias

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+if (process.env.NODE_ENV === "production") {
+  require("module-alias/register")
+}
+
 import { NestFactory } from "@nestjs/core"
+import { NestExpressApplication } from "@nestjs/platform-express"
 import { AppModule } from "./app.module"
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+
+  app.setGlobalPrefix("api")
   await app.listen(PORT, () => console.info(`[Nest] Server started on port = ${PORT}`))
 }
 bootstrap()
